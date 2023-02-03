b26d818ce39be18f119f2574354d1a4e4472f1fc
xinyi-gh <xinyi_li@sina.cn>
2023-02-03 17:48:16 +0800

    feat: finish part4 & 5, add partial scroll control

--- script.jsx
+++ script.jsx
@@ -1,7 +1,11 @@
 let indicatorNum = document.getElementById("indicatorNum").value;
 let objectNum = document.getElementById("objectNum").value;
 let indicatorWeights = [];
+let objectEvaluation = [];
+let objectRanks = [];
+let objectScores = [];
 let objectPage = 0;
+let modelStep = 0;
 
 function update_indicator_inputs() {
     var inputList = []
@@ -53,7 +57,59 @@ function update_visualModel() {
     const RECT_SPACE = 50;
 
     var SVGs = [];
-    if (renderIndicator && renderObject) { // draw the connecting lines
+    
+    let indicatorY = RECT_Y_BEGIN;
+    for (let i = 0; i < indicatorNum; i++) {
+        if (modelStep >= 0) {
+            SVGs.push([<rect
+                    width={RECT_WIDTH} height={RECT_HEIGHT} rx={20} ry={20}
+                    x={INDICATOR_RECT_X} y={indicatorY}
+                    fillOpacity={0} stroke={'#000000'} strokeWidth={5}
+                />,
+                <text
+                    x={INDICATOR_RECT_X + 10} y={indicatorY + 25} fontSize={20}>
+                    {document.getElementById(`indicator_${i}`).value}
+                </text>,
+            ]);
+        }
+        if (modelStep >= 1) {
+            SVGs.push(<text
+                x={INDICATOR_RECT_X - 40} y={indicatorY + 25} fontSize={20}>
+                {indicatorWeights[i]}
+            </text>)
+        }
+
+        indicatorY += RECT_SPACE;
+    }
+    
+    let objectY = RECT_Y_BEGIN
+    for (let i = 0; i < objectNum; i++) {
+        if (modelStep >= 2) {
+            SVGs.push(<rect
+                width={RECT_WIDTH} height={RECT_HEIGHT} rx={20} ry={20}
+                x={OBJECT_RECT_X} y={objectY}
+                fillOpacity={0} stroke={'#000000'} strokeWidth={5}
+            />);
+            SVGs.push(<text
+                x={OBJECT_RECT_X + 10} y={objectY + 25} fontSize={20}>
+                    {document.getElementById(`object_${i}`).value}
+            </text>);
+        }
+        if (modelStep >= 3) {
+            SVGs.push(<text
+                x={OBJECT_RECT_X + RECT_WIDTH + 20} y={objectY + 10} fontSize={20}>
+                    {objectRanks[i][0]}
+            </text>);
+            SVGs.push(<text
+                x={OBJECT_RECT_X + RECT_WIDTH + 10} y={objectY + 30} fontSize={20}>
+                    {objectScores[i]}
+            </text>);
+        }
+
+        objectY += RECT_SPACE;
+    }
+
+    if (modelStep >= 2) { // draw the connecting lines
         let indicatorY = RECT_Y_BEGIN;
         for (let i = 0; i < indicatorNum; i++) {
             let objectY = RECT_Y_BEGIN;
@@ -68,42 +124,7 @@ function update_visualModel() {
             indicatorY += RECT_SPACE;
         }
     }
-    if (renderIndicator) {
-        let indicatorY = RECT_Y_BEGIN;
-        for (let i = 0; i < indicatorNum; i++) {
-            SVGs.push(<rect
-                width={RECT_WIDTH} height={RECT_HEIGHT} rx={20} ry={20}
-                x={INDICATOR_RECT_X} y={indicatorY}
-                fillOpacity={0} stroke={'#000000'} strokeWidth={5}
-            />);
-            SVGs.push(<text
-                x={INDICATOR_RECT_X + 10} y={indicatorY + 25} fontSize={20}>
-                {document.getElementById(`indicator_${i}`).value}
-            </text>);
-            SVGs.push(<text
-                x={INDICATOR_RECT_X - 40} y={indicatorY + 25} fontSize={20}>
-                {indicatorWeights[i]}
-            </text>)
 
-            indicatorY += RECT_SPACE;
-        }
-    }
-    if (renderObject) {
-        let objectY = RECT_Y_BEGIN
-        for (let i = 0; i < objectNum; i++) {
-            SVGs.push(<rect
-                width={RECT_WIDTH} height={RECT_HEIGHT} rx={20} ry={20}
-                x={OBJECT_RECT_X} y={objectY}
-                fillOpacity={0} stroke={'#000000'} strokeWidth={5}
-            />);
-            SVGs.push(<text
-                x={OBJECT_RECT_X + 10} y={objectY + 25} fontSize={20}>
-                {document.getElementById(`object_${i}`).value}
-            </text>);
-
-            objectY += RECT_SPACE;
-        }
-    }
     ReactDOM.render(
         SVGs,
         document.getElementById("modelSVG")
@@ -187,7 +208,7 @@ function update_object_matrix() {
     );
 }
 
-function calculate() {
+function calculate_indicator_weight() {
     indicatorWeights = [];
     
     let n = indicatorNum;
@@ -196,6 +217,30 @@ function calculate() {
         for (let j = 0; j < n; j++) sum += parseFloat(document.getElementById(`indicatorMatrix_${i}_${j}`).value);
         let result = (sum + n/2 - 1) / (n * (n - 1));
         indicatorWeights.push(result.toFixed(2));
+    }
+}
+
+function calculate_object_score() {
+    objectEvaluation = [];
+    objectRanks = [];
+
+    for (let n = 0; n < objectNum; n++) {
+        let r = [0, 0, 0];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < indicatorNum; j++) {
+                r[i] += indicatorWeights[j] * document.getElementById(`objectMatrix${n}_${j}_${i}`).value;
+            }
+        }
+        objectEvaluation.push(r.map((score) => {return score.toFixed(2)}));
+
+        let max = Math.max.apply(null, r);
+        let rank = "";
+        if (r[0] == max) rank += "Excellent/";
+        if (r[1] == max) rank += "Good/";
+        if (r[2] == max) rank += "Poor/";
+        objectRanks.push(rank.slice(0, rank.length - 1)); // remove the last "/"
+
+        objectScores.push((3 * r[0] + 2 * r[1] + 1 * r[2]).toFixed(2));
     }
 }
 
@@ -237,13 +282,74 @@ function update_formula2() {
     let formula = [
         <mi>W</mi>,
         <mo>=</mo>,
-        <mo>{"{"}</mo>,
+        <mo>{"("}</mo>,
         contents,
-        <mo>{"}"}</mo>,
+        <mo>{")"}</mo>,
     ]
     ReactDOM.render(
         formula,
         document.getElementById("formula2")
+    );
+}
+
+function update_formula34() {
+    let w = indicatorWeights.map((weight) => {
+        return <mn>{weight}</mn>;
+    });
+    for (let i = 1; i < w.length; i += 2) w.splice(i, 0, <mo>,</mo>);
+
+    let r = objectEvaluation[0].map((score) => {
+        return <mn>{score}</mn>;
+    });
+    for (let i = 1; i < r.length; i += 2) r.splice(i, 0, <mo>,</mo>);
+
+    let wf = [];
+    for (let i = 0; i < indicatorNum; i++) {
+        wf.push(<mtr>
+            <mtd>
+                <mn>{document.getElementById(`objectMatrix0_${i}_0`).value}</mn>
+            </mtd>
+            <mtd>
+                <mn>{document.getElementById(`objectMatrix0_${i}_1`).value}</mn>
+            </mtd>
+            <mtd>
+                <mn>{document.getElementById(`objectMatrix0_${i}_2`).value}</mn>
+            </mtd>
+        </mtr>)
+    }
+    let formula = [
+        <mi>R</mi>,
+        <mo>=</mo>,
+        <mfenced>
+            <mrow>
+                {w}
+            </mrow>
+        </mfenced>,
+        <mo>&#xD7;</mo>,
+        <mo>{"["}</mo>,
+        <mtable>
+            {wf}
+        </mtable>,
+        <mo>{"]"}</mo>,
+        <mo>=</mo>,
+        <mfenced>
+            <mrow>
+                {r}
+            </mrow>
+        </mfenced>,
+    ];
+    ReactDOM.render(
+        formula,
+        document.getElementById("formula3")
+    );
+    
+    ReactDOM.render(
+        formula.slice(0, 3),
+        document.getElementById("formula4")
+    );
+    ReactDOM.render(
+        formula.slice(0, 3),
+        document.getElementById("formula4_")
     );
 }
 
@@ -265,14 +371,38 @@ function update() { // pay attention to the order
     update_object_inputs();
     update_indicator_matrix();
     update_object_matrix();
-    calculate();
+    calculate_indicator_weight();
+    calculate_object_score();
     update_formula1();
     update_formula2();
+    update_formula34();
     update_visualModel();
     ReactDOM.render(
         [objectPage + 1, "/", objectNum],
         document.getElementById("objectPage")
     );
+    ReactDOM.render(
+        objectRanks[0],
+        document.getElementById("object1Rank")
+    );
+    ReactDOM.render(
+        objectScores[0],
+        document.getElementById("object1Score")
+    );
 }
+window.onkeydown = () => {
+    alert(document.body.getBoundingClientRect().top);
+}
+window.onscroll = () => {
+    let t = document.body.getBoundingClientRect().top
+    document.getElementById("indicatorMatrix").parentElement.style.display = (t < -1000 && t > -2500) ? "" : "none";
+    document.getElementById("objectMatrix").parentElement.style.display = (t < -2500 && t > -4700) ? "" : "none";
 
+    let previous = modelStep
+    if (t < 0) modelStep = 0;
+    if (t < -2070) modelStep = 1;
+    if (t < -2700) modelStep = 2;
+    if (t < -3850) modelStep = 3;
+    if (modelStep != previous) update_visualModel();
+}
 update();
